dead8ab1f40669fd40afa33a39116b8a76e31546
Joelcloud1 <Joelephraimm@gmail.com>
2026-05-22 12:15:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def home():
+    return "Flask app is working on Azure!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
